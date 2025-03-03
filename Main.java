a83728beaa816eaf0dd6f603a55ea080cbf77857
pisnya1232 <gonaminuch@gmail.com>
2025-03-03 18:44:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public static void asin(String[] orgs) {
+Runnable a= new Threarnr();
+Thread b= new Thread(a);
+Runnable c= new Threar();
+Thread d= new Thread(c); 
+b.start();
+// d.start();
+}
---
+++ Threarrrr.java
@@ -0,0 +1,7 @@
+e.printStackTrace();
+}
+}
+For (int 1=8; 1c10;i++} (
+System.out.printin("asdfg");
+}
+}
